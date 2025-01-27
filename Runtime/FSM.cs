2eee86481b884540fe9b9a97224d34c509fbe007
Koen H <koen_hankel@hotmail.com>
2025-01-27 23:08:41 +0100

    chore: fixed bug in FSM

--- Runtime/FSM.cs
+++ Runtime/FSM.cs
@@ -10,10 +10,38 @@ namespace KJH.Utils
 
         public TState CurrentState { get { return currentState; } }
 
+        private bool isInitialized = false;
+
+        public FSM()
+        {
+
+        }
+
         public FSM(TState initialState)
         {
             currentState = initialState;
+        }
+
+        public void Initialize()
+        {
+            if (isInitialized)
+                throw new Exception("FSM is already initialized.");
+
+            if (currentState == null)
+                throw new Exception("No initial state is set.");
+
+            isInitialized = true;
+
             currentState.Enter();
+        }
+
+        public void Initialize(TState initialState)
+        {
+            if (isInitialized)
+                throw new Exception("FSM is already initialized.");
+
+            currentState = initialState;
+            Initialize();
         }
 
         public void ChangeState(TState newState)
